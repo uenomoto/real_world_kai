b1947a4cc201eef404044a4e350386bf66d4d12e
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-03 23:05:15 +0900

    ヘルスチェックは対策

---
+++ back/app/controllers/api/v1/health_check_controller.rb
@@ -0,0 +1,5 @@
+class HealthCheckController < ApplicationController
+  def index
+    render json: { status: 'OK' }, status: :ok
+  end
+end
--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'health_check', to: 'health_check#index'
       root 'articles#index'
       resources :articles, param: :slug, only: %i[show create update destroy]
     end
