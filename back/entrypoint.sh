b24c124e52987afc6d419efc37592250fb7feb84
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-04 16:37:11 +0900

    kome

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -7,8 +7,8 @@ rm -f /real_world_api/tmp/pids/server.pid
 # 本番環境の時だけ行うようにする
 if [ "$RAILS_ENV" = "production" ]; then
   # 本番環境（AWS ECS）への初回デプロイ時に利用
-  # 初回デプロイ後にコメントアウト
-  bundle exec rails db:create
+  # db:createは初回デプロイ後にコメントアウト
+  # bundle exec rails db:create
   bundle exec rails db:migrate
 fi
 # dockerfileのCMDの引数を実行する
