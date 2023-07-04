2e3031ac5795e754eacea58f1010bc51ea0c9608
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-04 22:25:14 +0900

    cors編集

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -11,7 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     if Rails.env.development?
       origins 'localhost:8000'
     else
-      origins 'real-world-kai.vercel.app'
+      origins 'pf-workoutmenu.com'
     end
 
     resource "*",
