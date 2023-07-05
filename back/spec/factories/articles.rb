814f6e04ea531681722fbead8c0f9a6fa13ad364
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-06 00:05:56 +0900

    konndokoso

--- back/spec/factories/articles.rb
+++ back/spec/factories/articles.rb
@@ -5,7 +5,6 @@
 FactoryBot.define do
   factory :article do
     sequence(:title) { |n| "Title #{n}" } # タイトルを連番で作成これでタイトルが重複しないようになる
-    sequence(:slug)
     description { 'Description' }
     body { 'Body' }
     created_at { Time.zone.now }
