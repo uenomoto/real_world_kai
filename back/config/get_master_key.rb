e23ffc91174b68518302bc17b724d7c89ffcbc88
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-07 12:57:30 +0900

    test

--- back/config/get_master_key.rb
+++ back/config/get_master_key.rb
@@ -3,3 +3,5 @@ require 'aws-sdk-ssm'
 ssm = Aws::SSM::Client.new(region: 'ap-northeast-1')
 resp = ssm.get_parameter(name: '/myapp/database/key', with_decryption: true)
 ENV['RAILS_MASTER_KEY'] = resp.parameter.value
+
+puts "Retrieved key: #{ENV['RAILS_MASTER_KEY']}"
