26594b9ee4b8a53eebd57c33450cd49c48b20eda
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-07 11:44:42 +0900

    test

--- back/config/application.rb
+++ back/config/application.rb
@@ -14,6 +14,7 @@ require 'action_mailbox/engine'
 require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
+require 'aws-sdk-ssm'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -48,5 +49,14 @@ module RealWorldApi
     end
 
     config.time_zone = 'Asia/Tokyo'
+
+    config.before_initialize do
+      ssm = Aws::SSM::Client.new(region: 'ap-northeast-1')
+      
+      %w[host username password name].each do |key|
+        resp = ssm.get_parameter(name: "/myapp/database/#{key}", with_decryption: true)
+        Rails.application.credentials.config[("db_#{key}").to_sym] = resp.parameter.value
+      end
+    end
   end
 end
